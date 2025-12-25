2c879b777959adb7979ece04e7161c807c608e8f
mirajhossen99 <siaumahmed013@gmail.com>
2025-12-25 12:18:18 +0600

    middleware added

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -1,7 +1,7 @@
 import { prisma } from "../config/db.js";
 
 const addToWatchlist = async (req, res) => {
-  const { movieId, status, rating, notes, userId } = req.body;
+  const { movieId, status, rating, notes } = req.body;
 
   // Veriry movie exists
   const movie = await prisma.movie.findUnique({
@@ -16,33 +16,60 @@ const addToWatchlist = async (req, res) => {
   const existingInWatchlist = await prisma.watchListItem.findUnique({
     where: {
       userId_movieId: {
-        userId: userId,
+        userId: req.user.id,
         movieId: movieId,
       },
     },
   });
 
   if (existingInWatchlist) {
-    return res.status(400).json({ error: "Movie already in the watchlist"})
+    return res.status(400).json({ error: "Movie already in the watchlist" });
   }
 
   const watchlistItem = await prisma.watchListItem.create({
     data: {
-        userId,
-        movieId,
-        status: status || "PLANNED",
-        rating,
-        notes,
-    }
+      userId: req.user.id,
+      movieId,
+      status: status || "PLANNED",
+      rating,
+      notes,
+    },
   });
 
   res.status(201).json({
     status: "Success",
     data: {
-        watchlistItem,
+      watchlistItem,
     },
-
   });
 };
 
-export {addToWatchlist}
+const removeFromWatchlist = async (req, res) => {
+  // Find watchlist item and verify owership
+  const watchlistItem = await prisma.watchListItem.findUnique({
+    where: { id: req.params.id },
+  });
+
+  if (!watchlistItem) {
+    return res.status(404).json({ error: "Watchlist item not found" });
+  }
+
+  // Ensure only owner can delete
+  if (watchlistItem.userId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ error: "Not allowed to update this watchlist item" });
+  }
+
+  await prisma.watchListItem.delete({
+    where: { id: req.params.id },
+  });
+
+  res.status(200).json({
+    status: "Success",
+    message: "Movie removed from watchlist",
+  });
+  
+};
+
+export { addToWatchlist, removeFromWatchlist };
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addToWatchlist } from "../controllers/watchlistController.js";
+import {
+  addToWatchlist,
+  removeFromWatchlist,
+} from "../controllers/watchlistController.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.use(authMiddleware);
 
+// router.post("/", authMiddleware, addToWatchlist);
 router.post("/", addToWatchlist);
-
-
+router.delete("/:id", removeFromWatchlist);
 
 export default router;
