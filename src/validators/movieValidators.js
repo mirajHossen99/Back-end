b1d158944010483ca34473f4d7f9d58d4edfac25
mirajhossen99 <siaumahmed013@gmail.com>
2025-12-27 07:30:32 +0600

    updateMovieSchema validators added

--- src/validators/movieValidators.js
+++ src/validators/movieValidators.js
@@ -11,4 +11,14 @@ const addMovieSchema = z.object({
 
 });
 
-export { addMovieSchema }
+const updateMovieSchema = z.object({
+  title: z.string(),
+  overview: z.string("Overwiew must be a string").optional(),
+  releaseYear: z.number().int("Release year must be an integer"),
+  genres: z.array(z.string("Genres must be contain")),
+  runtime: z.number().int("It must be an integer").optional(),
+  posterUrl: z.string("Post url must be a string").optional(),
+
+});
+
+export { addMovieSchema, updateMovieSchema }
